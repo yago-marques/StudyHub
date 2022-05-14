8bc75441ebe7e725fb3c0982a5205cfb875d0b3b
Yago Marques <yagovictormarques@gmail.com>
2022-05-14 20:17:25 -0300

    login crud with firebase

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -8,7 +8,7 @@ export function AppRoutes() {
       <Route path="/home" element={<Home />} />
       <Route path="/login" element={<SignIn />} />
 
-      <Route path="/*" element={<Navigate to="/home" />} />
+      <Route path="/" element={<Navigate to="/login" />} />
     </Routes>
   )
 }
