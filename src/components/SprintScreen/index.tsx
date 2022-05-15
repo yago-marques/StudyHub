fd3cfd88caa6879ee0c99f56af436ebb383762b7
Yago Marques <yagovictormarques@gmail.com>
2022-05-15 14:44:23 -0300

    register interface

--- src/components/SprintScreen/index.tsx
+++ src/components/SprintScreen/index.tsx
@@ -1,9 +1,23 @@
 import { Container } from "./style"
+import { useNavigate } from "react-router-dom"
 
 export function SprintScreen() {
+  const navigate = useNavigate()
+
   return (
     <Container>
       <div className="loader"></div>
+      {setTimeout(() => {
+        <button
+          onClick={() => {
+            localStorage.clear()
+            navigate("/home")
+          }}
+        >
+          Cancelar
+        </button>
+      }, 3000)}
+      
     </Container>
   )
 }
--- src/components/SprintScreen/style.ts
+++ src/components/SprintScreen/style.ts
@@ -2,11 +2,13 @@ import styled from "styled-components"
 
 export const Container = styled.section`
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
   height: 100vh;
   
   .loader {
+    margin-bottom: 2rem;
     height: 8rem;
     width: 8rem;
     border: 5px solid transparent;
@@ -20,6 +22,24 @@ export const Container = styled.section`
     }
     to {
       transform: rotate(360deg);
+    }
+  }
+
+  button {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border: none;
+    background: var(--yellow);
+    height: 3rem;
+    width: 7rem;
+    font-size: 1rem;
+    color: var(--bg-color);
+    border-radius: 0.3rem;
+    transition-duration: 0.3s;
+    font-weight: 500;
+    &:hover {
+      filter: brightness(0.8);
     }
   }
 `
